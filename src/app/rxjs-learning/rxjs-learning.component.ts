c74cb899e92d9180eb8e7da12e242ad97257b9e9
Kumar <894915@cognizant.com>
2023-02-25 15:16:02 +0530

    Rxjs-Observable example

---
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {  Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  templateUrl: './rxjs-learning.component.html',
+  styleUrls: ['./rxjs-learning.component.css']
+})
+export class RxjsLearningComponent implements OnInit {
+  agents!: Observable<string>;
+  agentName :any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.agents = new Observable(
+      function(observer){
+          try{
+            observer.next('Ram');
+            setInterval(()=>{
+              observer.next('Mark');
+            },3000);
+
+            setInterval(()=>{
+              observer.next('Sita');
+            },6000);
+          }
+          catch(e){
+            observer.error(e);
+          }
+      }
+    );
+    this.agents.subscribe(data =>{
+      //console.log(data);
+      this.agentName = data ;
+    });
+  }
+
+}
